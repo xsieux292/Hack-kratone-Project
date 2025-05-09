24d07c8d4bb3ba5957cbbe667b71568486a38e31
xsieux292 <xsiseux292@gmail.com>
2025-05-09 13:27:23 +0700

    Update script

--- script.js
+++ script.js
@@ -95,8 +95,8 @@ function CheckResult(){
 
 //ฟังก์ชันสำหรับสร้างจำนวนชุดของหวยว่ามีกี่ชุด p1, p2, p3, p4, p5, pdf3, pdl3, pdl2
 const GetLotteryResult = () => {
-    const number_prize = [1, 5, 10, 15 ,20, 2, 2, 2, 1];
-    const prizeTypes = ['prize_1', 'prize_2', 'prize_3', 'prize_4', 'prize_5', 'prize_1_same', 'prize_3_d_first', 'prize_3_d_last', 'prize_2_d_last'];
+    const number_prize = [1, 1, 1, 1 ,1, ];
+    const prizeTypes = ['prize_1', 'prize_2', 'prize_3', 'prize_4', 'prize_5'];
   
     return prizeTypes.reduce((acc, prize, index) => {
       acc[prize] = Array.from({ length: number_prize[index] }, generateRandomNumber);
