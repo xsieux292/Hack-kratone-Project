04578c2d385f412dcec5960b70a78f0081daa835
xsieux292 <xsiseux292@gmail.com>
2025-05-15 20:08:50 +0700

    Update index.html & input.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
         roboto: ['Kanit', 'sans-serif'],
       },
     },
+    screens: {
+      'xs': '420px',   // กำหนด breakpoint xs เองที่ 420px
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
